76dd7b2e69700801ea33c7de95dd5e9da19cb621
youssifshadow <gooj85183@gmail.com>
2024-05-14 14:22:27 +0300

    Update rpg-mining.js

--- plugins/rpg-mining.js
+++ plugins/rpg-mining.js
@@ -1,4 +1,4 @@
-//import db from '../lib/database.js'
+/*import db from '../lib/database.js'
 
 let handler = async (m, { conn }) => {
 
@@ -27,4 +27,4 @@ function msToTime(duration) {
   seconds = (seconds < 10) ? "0" + seconds : seconds
 
   return hours + " hora(s) " + minutes + " minuto(s) " + seconds + " segundo(s)" 
-}
+}*/
