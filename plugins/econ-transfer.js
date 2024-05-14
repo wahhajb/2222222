8f3b1e0c9484db46f93d91327876f62f651c26ce
youssifshadow <gooj85183@gmail.com>
2024-05-14 06:49:57 +0300

    Update econ-transfer.js

--- plugins/econ-transfer.js
+++ plugins/econ-transfer.js
@@ -1,45 +1,46 @@
-const items = ['diamond', 'exp']
+const items = [
+    'limit', 'exp',
+]
 let confirmation = {}
-
 async function handler(m, { conn, args, usedPrefix, command }) {
-    if (confirmation[m.sender]) return m.reply('estas haciendo una transferencia')
+    if (confirmation[m.sender]) return m.reply('*⚠️ أنت تقوم بالتحويل*')
     let user = global.db.data.users[m.sender]
     const item = items.filter(v => v in user && typeof user[v] == 'number')
-    let lol = `✳️ الاستخدام الصحيح للامر 
-*${usedPrefix + command}*   [كميه] [@منشن]
+    let lol = `*⚠️ استخدام الأمر*
+${usedPrefix + command}  [النوع] [العدد] [@user]
 
-📌 Contoh : 
-*${usedPrefix + command}* exp 65 @${m.sender.split('@')[0]}
+📝 مثال : ${usedPrefix + command} exp 65 @201012531172
 
-📍 Item yang dapat ditransfer
-┌──────────────
-▢ *diamond* = Diamante 💎
-▢ *exp* = Experiencia 🆙
-└──────────────
-`.trim()
+
+📍 العناصر القابلة للتحويل
+
+💎 *حد* = الماس
+✨ *اكسبي* = خبره`.trim()
     const type = (args[0] || '').toLowerCase()
-    if (!item.includes(type)) return conn.reply(m.chat, lol, m, { mentions: [m.sender] })
+    if (!item.includes(type)) return m.reply(lol)
     const count = Math.min(Number.MAX_SAFE_INTEGER, Math.max(1, (isNumber(args[1]) ? parseInt(args[1]) : 1))) * 1
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[2] ? (args[2].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
-    if (!who) return m.reply('✳️ Taguea pengguna')
-    if (!(who in global.db.data.users)) return m.reply(`✳️ Pengguna tidak ada dalam database saya`)
-    if (user[type] * 1 < count) return m.reply(`✳️  *${type}*  tidak cukup untuk mentransfer`)
+    if (!who) return m.reply('*⚠️ ضع منشن على المستخدم*')
+    if (!(who in global.db.data.users)) return m.reply(`*⚠️ المستخدم ${who} ليس في قاعدة البيانات*`)
+    if (user[type] * 1 < count) return m.reply(`*⚠️  ${type}  غير كافية للنقل*`)
     let confirm = `
-Apakah Anda yakin ingin mentransfer *${count}* _*${type}*_ a  *@${(who || '').replace(/@s\.whatsapp\.net/g, '')}* ? 
+¿هل أنت متأكد أنك تريد النقل *${count}* ${type} a  *@${(who || '').replace(/@s\.whatsapp\.net/g, '')}* ? 
 
-- Punya  *60s* 
-_Merespon *si* o *no*_
+يملك  *60* s
+
+اكتب: (si) لتصحيح الأمر
+اكتب: (no) للإلغاء
 `.trim()
-   
-    //conn.sendButton(m.chat, confirm, fgig, null, [['si'], ['no']], m, { mentions: [who] })
-    m.reply(confirm, null, { mentions: [who] })
+    let c = '𝙶𝚄𝚃𝚂\x20-\x20𝙱𝙾𝚃'
+    await conn.reply(m.chat, confirm, m, { mentions: [who] })
+    //conn.sendButton(m.chat, confirm, c, null, [['si'], ['no']], m, { mentions: [who] })
     confirmation[m.sender] = {
         sender: m.sender,
         to: who,
         message: m,
         type,
         count,
-        timeout: setTimeout(() => (m.reply('⏳ Waktu sudah habis'), delete confirmation[m.sender]), 60 * 1000)
+        timeout: setTimeout(() => (m.reply('انتهى الوقت'), delete confirmation[m.sender]), 60 * 1000)
     }
 }
 
@@ -51,34 +52,42 @@ handler.before = async m => {
     if (m.id === message.id) return
     let user = global.db.data.users[sender]
     let _user = global.db.data.users[to]
-    if (/no?/g.test(m.text.toLowerCase())) {
+    if (/^No|no$/i.test(m.text) ) { 
+    //if (/no?/g.test(m.text.toLowerCase())) {
         clearTimeout(timeout)
         delete confirmation[sender]
-        return m.reply('✅ Transfer dibatalkan')
+        return m.reply('*الغيت*')
     }
-    if (/si?/g.test(m.text.toLowerCase())) {
+    if (/^Si|si$/i.test(m.text) ) { 
+   // if (/si?/g.test(m.text.toLowerCase())) {
         let previous = user[type] * 1
         let _previous = _user[type] * 1
         user[type] -= count * 1
         _user[type] += count * 1
-        if (previous > user[type] * 1 && _previous < _user[type] * 1) m.reply(`✅ Pemindahan \n\n*${count}* *${type}*  a @${(to || '').replace(/@s\.whatsapp\.net/g, '')}`, null, { mentions: [to] })
+        if (previous > user[type] * 1 && _previous < _user[type] * 1) m.reply(`✅ نـــقــل نـــاجــح لـ \n\n*${count}* *${type}*  a @${(to || '').replace(/@s\.whatsapp\.net/g, '')}`, null, { mentions: [to] })
         else {
             user[type] = previous
             _user[type] = _previous
-            m.reply(`❎ Pengalihan kesalahan *${count}* ${type} a *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
+            m.reply(`حـــدث خـــطــأ أثـــنــاء الـــنــقـل *${count}* ${type} to *@${(to || '').replace(/@s\.whatsapp\.net/g, '')}*`, null, { mentions: [to] })
         }
         clearTimeout(timeout)
         delete confirmation[sender]
     }
 }
 
-handler.help = ['transfer'].map(v => v + ' [tipo] [monto] [@tag]')
-handler.tags = ['econ']
-handler.command = ['payxp','شراء', 'transfer', 'darxp','dardi',]
+handler.help = ['transfer'].map(v => v + ' [tipo] [cantidad] [@tag]')
+handler.tags = ['rg']
+handler.command = ['payxp', 'تحويل', 'darxp'] 
 
 handler.disabled = false
 
 export default handler
+
+function special(type) {
+    let b = type.toLowerCase()
+    let special = (['common', 'uncommon', 'mythic', 'legendary', 'pet'].includes(b) ? ' Crate' : '')
+    return special
+}
 
 function isNumber(x) {
     return !isNaN(x)
