e75dd31886feba478174edbfc1e4d771104c0fcf
youssifshadow <gooj85183@gmail.com>
2024-05-14 14:21:43 +0300

    Update and rename rpg-minar.js to تعدين.js

---
+++ plugins/تعدين.js
@@ -0,0 +1,29 @@
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn }) => {
+
+  let hasil = Math.floor(Math.random() * 5000)
+  let time = global.db.data.users[m.sender].lastmiming + 14400000
+  if (new Date - global.db.data.users[m.sender].lastmiming < 14400000) throw `*انـــتــظـر [⏳]${msToTime(time - new Date())}*`
+  global.db.data.users[m.sender].exp += hasil
+  m.reply(`مـــبــروك لـــقــد حـــصــلـت عـــلــي *${hasil}*`)
+  global.db.data.users[m.sender].lastmiming = new Date * 1
+}
+handler.help = ['mine']
+handler.tags = ['econ']
+handler.command = ['تعدين', 'miming', 'mine'] 
+
+export default handler
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + " ساعات(s) " + minutes + " دقيقه(s) " + seconds + " ثانيه(s)" 
+}
