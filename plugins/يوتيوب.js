d630cf0e8f3296c5383e1b366ef869bd62492d96
youssifshadow <gooj85183@gmail.com>
2024-05-14 07:24:01 +0300

    Update يوتيوب.js

--- plugins/يوتيوب.js
+++ plugins/يوتيوب.js
@@ -1,8 +1,8 @@
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw '*هذا الامر خاص بالتحميل من اليوتوب* مثال : \n *.yt* https://youtube.com/@MR_SA3A?si=DVhTpF2J9Se-JZzg'
-await m.reply(`*جاري تحميل الفيديو المرجو الانتظار قليلا يا عزيزي* `)
+if (!args[0]) throw '*✪┇اكـتـب الامـر ثـم ضــع رابـط فــيـديـو الــيـوتـيـوب┇*\n\n*✧┇مـثـال☜┇*\n*.يوتيوب https://youtu.be/Jz3T33QBbBk?si=RCHcKKu6Yj5SgIvK*'
+await m.reply(`*✪┇جــاري الــتـحـميـل يــرجـى الانــتـظـار*`)
 try {
 let qu = args[1] || '720'
 let q = qu + 'p'
@@ -11,7 +11,7 @@ const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async
 const dl_url = await yt.video[q].download()
 const ttl = await yt.title
 const size = await yt.video[q].fileSizeH
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `▢: ${ttl}\n▢ 𝙿: ${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: '*✥─━─━─╃⌬〔🍁〕⌬╄─━─━─✥*\n*تــم تــنــفـذ الــطـلـب بــنـجـاح✅*\n\n*┇${ttl}*\n*✥─━─━─╃⌬〔🍁〕⌬╄─━─━─✥*`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
 } catch {
 try {
 let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
@@ -22,7 +22,7 @@ let n3 = lolh.result.size
 let n4 = lolh.result.thumbnail
 await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃: ${n}\n▢ 𝙿: ${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
 } catch {
-await conn.reply(m.chat, 'تعذر تحميل الفيديو راسل صاصاا لتعرف السبب', m)}
+await conn.reply(m.chat, '*تـــعــذر تـــحــميـل الـــفــيـديـو*', m)}
 }}
 handler.command = /^video|yt|يوتيوب$/i
 export default handler
