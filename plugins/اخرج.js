fc9967b9cb29653f8604aad5b8b92cb87a58317c
youssifshadow <gooj85183@gmail.com>
2024-05-14 14:48:54 +0300

    Update اخرج.js

--- plugins/اخرج.js
+++ plugins/اخرج.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, command }) => {
-await m.reply('*انا اسف ان كنت ازعجتكم , وداعا للجميع احبكم! (≧ω≦)ゞ*') 
+await m.reply('*┇ســيـخـرج الــبـوت بــأمـر مــن الــمـطـور┇*') 
 await  conn.groupLeave(m.chat)}
 handler.command = /^(out|leavegc|اخرج|برا)$/i
 handler.group = true
